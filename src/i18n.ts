4b418fa8dce68af8d4f45fdde66bb881278f2a6e
Rastislav Wagner <rawagner@redhat.com>
2021-11-23 12:28:01 +0100

    Add GPU health

---
+++ src/i18n.ts
@@ -0,0 +1,9 @@
+// IMPORTANT: This file adds comments recognized by the react-i18next-parser so that
+// labels declared in console-extensions.json are added to the message catalog.
+
+// TODO: We should make the custom i18next JSON lexer available to plugins so
+// that they do not need this file.
+
+// t('plugin__console-plugin-gpu~GPU Power usage')
+// t('plugin__console-plugin-gpu~GPU Encoder/Decoder')
+// t('plugin__console-plugin-gpu~GPU')
