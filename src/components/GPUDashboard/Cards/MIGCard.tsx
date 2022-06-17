rawagner/console-plugin-gpu

GPU Dashboard enhancements: Handle different MIG strategies Improve loading/error states Update prometheus queries

src/components/GPUDashboard/Cards/MIGCard.tsx
@@ -1,0 +1,153 @@
+import { ChartBar, ChartStack, ChartThemeColor, ChartTooltip } from '@patternfly/react-charts';
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Skeleton,
+  Split,
+  SplitItem,
+  Badge,
+  Brand,
+  Stack,
+  StackItem,
+  CardTitle,
+} from '@patternfly/react-core';
+import * as React from 'react';
+import { humanizeBinaryBytes } from '../../../utils/units';
+import { useGPUNode } from '../../../hooks/use-gpus-info';
+import { useTranslation } from '../../../i18n';
+import { GPUDashboardContext } from '../GPUDashboardContext';
+import NotAvailable from '../../NotAvailable';
+import { useRefWidth } from '../../../hooks/use-ref-width';
+import { getMIGStrategy, getMixedMIGTypes } from '../../../utils/mig';
+
+import nvidiaLogo from '../../../../icons/Nvidia_logo.svg';
+import './MIGCard.css';
+
+const MIGCard = () => {
+  const { t } = useTranslation();
+  const { gpusLoading, selectedGPU, gpusError, gpus } = React.useContext(GPUDashboardContext);
+  const [gpuNode, gpuNodeLoaded, gpuNodeError] = useGPUNode(selectedGPU);
+  const [containerRef, width] = useRefWidth();
+
+  const loading = gpusLoading || !gpuNodeLoaded;
+  const error = !loading && (gpusError || gpuNodeError || !selectedGPU);
+
+  const chartBars = [];
+
+  let multiInstance = false;
+  const migStrategy = getMIGStrategy(gpuNode);
+
+  if (migStrategy === 'mixed') {
+    const migTypes = gpuNode ? getMixedMIGTypes(gpuNode) : {};
+
+    multiInstance = Object.keys(migTypes).length > 1;
+
+    Object.keys(migTypes).forEach((key) => {
+      for (let i = 0; i < migTypes[key].count / gpus.length; i++) {
+        chartBars.push(
+          <ChartBar
+            alignment="middle"
+            key={key}
+            data={[
+              {
+                name: `Instance ${i + 1}`,
+                x: '0',
+                y: migTypes?.[key]?.memory,
+                label: `Instance size: ${
+                  humanizeBinaryBytes(migTypes?.[key]?.memory as number, 'MiB').string
+                }`,
+              },
+            ]}
+            barWidth={15}
+            padding={0}
+          />,
+        );
+      }
+    });
+  } else {
+    const count = gpuNode?.metadata?.labels?.['nvidia.com/gpu.count'];
+    const memory = gpuNode?.metadata?.labels?.['nvidia.com/gpu.memory'];
+    if (count && memory) {
+      multiInstance = parseInt(count) > 1;
+      for (let i = 0; i < parseInt(count) / gpus.length; i++) {
+        chartBars.push(
+          <ChartBar
+            key={i}
+            data={[
+              {
+                name: `Instance ${i + 1}`,
+                x: '0',
+                y: memory,
+                label: `Instance size: ${humanizeBinaryBytes(parseInt(memory), 'MiB').string}`,
+              },
+            ]}
+            barWidth={15}
+            padding={0}
+          />,
+        );
+      }
+    }
+  }
+
+  return (
+    <Card className="ng-mig-card ng-mig-card--full-height">
+      {error ? (
+        <CardBody className="ng-mig-card--full-height">
+          <NotAvailable />
+        </CardBody>
+      ) : (
+        <>
+          <CardHeader>
+            {loading ? (
+              <Skeleton screenreaderText={t('Loading card content')} width="30%" />
+            ) : (
+              <Split hasGutter>
+                <SplitItem>
+                  <Brand src={nvidiaLogo} alt="Nvidia logo" className="ng-mig-card__logo" />
+                </SplitItem>
+                <SplitItem>
+                  <Stack>
+                    <StackItem>
+                      <Split hasGutter>
+                        <SplitItem>
+                          <CardTitle>{selectedGPU?.modelName}</CardTitle>
+                        </SplitItem>
+                        <SplitItem>
+                          <Badge isRead>
+                            {multiInstance ? t('Multi-instance') : t('Single-instance')}
+                          </Badge>
+                        </SplitItem>
+                      </Split>
+                    </StackItem>
+                    {migStrategy && <StackItem>{`${t('MIG strategy')}: ${migStrategy}`}</StackItem>}
+                  </Stack>
+                </SplitItem>
+              </Split>
+            )}
+          </CardHeader>
+          <CardBody>
+            <div ref={containerRef} className="ng-mig-card__chart">
+              {loading ? (
+                <Skeleton screenreaderText={t('Loading card content')} />
+              ) : (
+                <ChartStack
+                  horizontal
+                  padding={0}
+                  height={15}
+                  themeColor={ChartThemeColor.multiUnordered}
+                  width={width}
+                  labelComponent={<ChartTooltip />}
+                >
+                  {chartBars}
+                </ChartStack>
+              )}
+            </div>
+          </CardBody>
+        </>
+      )}
+    </Card>
+  );
+};
+
+export default MIGCard;

src/components/GPUDashboard/Cards/MetricsCard.tsx
@@ -1,0 +1,154 @@
+import * as React from 'react';
+import { Card, CardBody, CardTitle, Skeleton, CardFooter, Bullseye } from '@patternfly/react-core';
+import {
+  usePrometheusPoll,
+  PrometheusEndpoint,
+  HumanizeResult,
+  Humanize,
+} from '@openshift-console/dynamic-plugin-sdk';
+import {
+  ChartArea,
+  ChartDonutUtilization,
+  ChartGroup,
+  ChartVoronoiContainer,
+} from '@patternfly/react-charts';
+import { Link } from 'react-router-dom';
+import {
+  getPrometheusResultScalarNumber,
+  getPrometheusResultTimeSerie,
+} from '../../../utils/prometheus';
+import { useTranslation } from '../../../i18n';
+import { timeFormatter } from '../../../utils/time';
+import { useRefWidth } from '../../../hooks/use-ref-width';
+
+import './MetricsCard.css';
+
+type MetricsCardProps = {
+  query: string;
+  loading: boolean;
+  humanize: Humanize;
+  title: string;
+  ariaTitle: string;
+  ariaDesc: string;
+  ariaRangeTitle: string;
+  error: boolean;
+  maxDomain?: number;
+  maxQuery?: string;
+  defaultUnit?: string;
+};
+
+const MetricsCard: React.FC<MetricsCardProps> = ({
+  loading,
+  ariaTitle,
+  title,
+  maxDomain = 0,
+  ariaDesc,
+  ariaRangeTitle,
+  humanize,
+  defaultUnit,
+  error,
+  ...rest
+}) => {
+  const { t } = useTranslation();
+  const [containerRef, width] = useRefWidth();
+
+  // do not run the queries if there is an error
+  const [query, maxQuery] =
+    error || loading ? [undefined, undefined] : [`${rest.query}[60m:5m]`, rest.maxQuery];
+  const [result, queryLoading, queryError] = usePrometheusPoll({
+    endpoint: PrometheusEndpoint.QUERY,
+    query,
+  });
+
+  const [maxResult, maxLoading, maxError] = usePrometheusPoll({
+    endpoint: PrometheusEndpoint.QUERY,
+    // do not run the query if there is an error
+    query: maxQuery,
+  });
+
+  const timeSerie = getPrometheusResultTimeSerie(result);
+  const max = maxDomain || getPrometheusResultScalarNumber(maxResult);
+
+  loading = loading || queryLoading || maxLoading || !result || (!!maxQuery && !maxResult);
+
+  error =
+    error ||
+    !!queryError ||
+    !!maxError ||
+    (!loading && (timeSerie === undefined || max === undefined));
+  const timeData = timeSerie?.map((pair) => ({
+    x: pair.time,
+    y: pair.value,
+  }));
+
+  let actualInPercents: number | undefined;
+  const latestValue = timeData?.[timeData?.length - 1].y;
+  let currentValue: HumanizeResult | undefined = undefined;
+  if (!error && latestValue !== undefined) {
+    currentValue = humanize(latestValue, defaultUnit);
+    actualInPercents = max ? (latestValue / max) * 100 : 0;
+  }
+
+  return (
+    <Card className="ng-metrics-card">
+      <CardTitle className="pf-u-text-align-center">{title}</CardTitle>
+      <CardBody>
+        {loading && !error ? (
+          <Bullseye>
+            <Skeleton shape="circle" width="70%" screenreaderText={t('Loading card content')} />
+          </Bullseye>
+        ) : (
+          <div ref={containerRef} className="ng-metrics-card__graph">
+            <ChartDonutUtilization
+              ariaDesc={title}
+              ariaTitle={ariaTitle}
+              constrainToVisibleArea={true}
+              data={error ? { x: 0 } : { x: title, y: actualInPercents }}
+              labels={({ datum }) => (datum.x ? `${title}: ${currentValue?.string}` : null)}
+              title={error ? t('Not available') : `${currentValue?.string}`}
+              thresholds={[{ value: 60 }, { value: 80 }]}
+              width={width}
+            />
+          </div>
+        )}
+      </CardBody>
+      <CardFooter className="ng-metrics-card">
+        {loading && !error ? (
+          <div style={{ height: '70px' }}>
+            <Skeleton width="100%" height="100%" screenreaderText={t('Loading card content')} />
+          </div>
+        ) : (
+          !error && (
+            <div className="ng-metrics-card__graph">
+              <Link to={`/monitoring/query-browser?query0=${rest.query}`}>
+                <ChartGroup
+                  ariaDesc={ariaDesc}
+                  ariaTitle={ariaRangeTitle}
+                  containerComponent={
+                    <ChartVoronoiContainer
+                      activateData={false}
+                      voronoiDimension="x"
+                      labels={({ datum }) =>
+                        `${
+                          humanize(datum.y, defaultUnit, currentValue?.unit).string
+                        } at ${timeFormatter.format(new Date(datum.x * 1000))}`
+                      }
+                    />
+                  }
+                  height={60}
+                  width={width}
+                  maxDomain={{ y: timeData ? Math.max(...timeData.map((td) => td.y)) * 1.2 : 0 }}
+                  padding={0}
+                >
+                  <ChartArea data={timeData} scale={{ x: 'time', y: 'linear' }} height={90} />
+                </ChartGroup>
+              </Link>
+            </div>
+          )
+        )}
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default MetricsCard;

src/components/GPUDashboard/GPUSelector.tsx
@@ -1,0 +1,53 @@
+import * as React from 'react';
+import { SelectOption, DropdownPosition, Select, Card, Spinner } from '@patternfly/react-core';
+import { GPUDashboardContext } from './GPUDashboardContext';
+import { useTranslation } from '../../i18n';
+
+const GPUSelector: React.FC = () => {
+  const { t } = useTranslation();
+  const { selectedGPU, gpus, setSelectedGPU, gpusLoading, gpusError } =
+    React.useContext(GPUDashboardContext);
+  const [isOpen, setOpen] = React.useState(false);
+
+  const isDisabled = !gpus?.length || !!gpusError || gpusLoading;
+
+  let items: React.ReactElement[];
+
+  if (gpusLoading) {
+    items = [
+      <SelectOption key="disabled" value={t('Loading GPUs')}>
+        <Spinner size="md" /> {t('Loading GPUs')}
+      </SelectOption>,
+    ];
+  } else if (!gpus?.length || gpusError) {
+    items = [<SelectOption key="disabled" value={t('No GPU found')} />];
+  } else {
+    items = gpus.map((gpu) => (
+      <SelectOption
+        key={gpu.uuid}
+        value={gpu.uuid}
+        description={`Node: ${gpu.nodeName}, Model: ${gpu.modelName}`}
+      />
+    ));
+  }
+
+  return (
+    <Card>
+      <Select
+        onSelect={(e, selection: string) => {
+          setSelectedGPU(selection);
+          setOpen(false);
+        }}
+        onToggle={setOpen}
+        isOpen={isOpen}
+        isDisabled={isDisabled}
+        position={DropdownPosition.right}
+        selections={selectedGPU?.uuid}
+      >
+        {items}
+      </Select>
+    </Card>
+  );
+};
+
+export default GPUSelector;

src/hooks/use-gpus-info.ts
@@ -1,0 +1,55 @@
+import {
+  PrometheusEndpoint,
+  useK8sWatchResource,
+  usePrometheusPoll,
+} from '@openshift-console/dynamic-plugin-sdk';
+import * as _ from 'lodash';
+import { Node } from '../resources';
+import { useDeepCompareMemoize } from './use-deep-memoize';
+
+export type GPUInfo = {
+  uuid: string;
+  modelName: string;
+  nodeName: string;
+};
+
+/**
+ * Get a list of all GPUs in the cluster and their details.
+ *
+ */
+export const useGPUsInfo = (): [GPUInfo[], /* loading */ boolean, /* error */ unknown] => {
+  const [result, loading, error] = usePrometheusPoll({
+    endpoint: PrometheusEndpoint.QUERY,
+    query: 'DCGM_FI_PROF_GR_ENGINE_ACTIVE',
+  });
+
+  const gpus = useDeepCompareMemoize(
+    _.uniqBy(
+      result?.data?.result?.map((res) => ({
+        uuid: res.metric?.UUID,
+        modelName: res.metric?.modelName,
+        nodeName: res.metric?.Hostname,
+      })),
+      (g) => g.uuid,
+    ),
+  );
+
+  return [gpus, loading, error];
+};
+
+export const useGPUNode = (gpu?: GPUInfo): [Node | undefined, boolean, unknown] => {
+  const [node, nodeLoaded, nodeError] = useK8sWatchResource<Node>(
+    gpu?.nodeName
+      ? {
+          groupVersionKind: {
+            kind: 'Node',
+            version: 'v1',
+          },
+          name: gpu.nodeName,
+          isList: false,
+        }
+      : null,
+  );
+
+  return [node, nodeLoaded, nodeError];
+};

rh-ecosystem-edge/console-plugin-nvidia-gpu

src/components/GPUDashboard/Cards/MIGCard.tsx
@@ -1,0 +1,154 @@
+import { ChartBar, ChartStack, ChartThemeColor, ChartTooltip } from '@patternfly/react-charts';
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Skeleton,
+  Split,
+  SplitItem,
+  Badge,
+  Brand,
+  Stack,
+  StackItem,
+  CardTitle,
+} from '@patternfly/react-core';
+import * as React from 'react';
+import { humanizeBinaryBytes } from '../../../utils/units';
+import { useGPUNode } from '../../../hooks/use-gpus-info';
+import { useTranslation } from '../../../i18n';
+import { GPUDashboardContext } from '../GPUDashboardContext';
+import NotAvailable from '../../NotAvailable';
+import { useRefWidth } from '../../../hooks/use-ref-width';
+import { getMIGStrategy, getMixedMIGTypes } from '../../../utils/mig';
+
+import nvidiaLogo from '../../../../icons/Nvidia_logo.svg';
+import './MIGCard.css';
+
+const MIGCard = () => {
+  const { t } = useTranslation();
+  const { gpusLoaded, selectedGPU, gpusError, gpus } = React.useContext(GPUDashboardContext);
+  const [gpuNode, gpuNodeLoaded, gpuNodeError] = useGPUNode(selectedGPU);
+  const [containerRef, width] = useRefWidth();
+
+  const loading = !gpusLoaded || !gpuNodeLoaded;
+  const error = !loading && (gpusError || gpuNodeError || !selectedGPU);
+
+  const chartBars = [];
+
+  let multiInstance = false;
+  const migStrategy = getMIGStrategy(gpuNode);
+
+  if (migStrategy === 'mixed') {
+    const migTypes = gpuNode ? getMixedMIGTypes(gpuNode) : {};
+
+    multiInstance =
+      Object.keys(migTypes).length > 1 || Object.values(migTypes).some(({ count }) => count > 1);
+
+    Object.keys(migTypes).forEach((key) => {
+      for (let i = 0; i < migTypes[key].count / gpus.length; i++) {
+        chartBars.push(
+          <ChartBar
+            alignment="middle"
+            key={key}
+            data={[
+              {
+                name: `Instance ${i + 1}`,
+                x: '0',
+                y: migTypes?.[key]?.memory,
+                label: `Instance size: ${
+                  humanizeBinaryBytes(migTypes?.[key]?.memory as number, 'MiB').string
+                }`,
+              },
+            ]}
+            barWidth={15}
+            padding={0}
+          />,
+        );
+      }
+    });
+  } else {
+    const count = gpuNode?.metadata?.labels?.['nvidia.com/gpu.count'];
+    const memory = gpuNode?.metadata?.labels?.['nvidia.com/gpu.memory'];
+    if (count && memory) {
+      multiInstance = parseInt(count) > 1;
+      for (let i = 0; i < parseInt(count) / gpus.length; i++) {
+        chartBars.push(
+          <ChartBar
+            key={i}
+            data={[
+              {
+                name: `Instance ${i + 1}`,
+                x: '0',
+                y: memory,
+                label: `Instance size: ${humanizeBinaryBytes(parseInt(memory), 'MiB').string}`,
+              },
+            ]}
+            barWidth={15}
+            padding={0}
+          />,
+        );
+      }
+    }
+  }
+
+  return (
+    <Card className="ng-mig-card ng-mig-card--full-height">
+      {error ? (
+        <CardBody className="ng-mig-card--full-height">
+          <NotAvailable />
+        </CardBody>
+      ) : (
+        <>
+          <CardHeader>
+            {loading ? (
+              <Skeleton screenreaderText={t('Loading card content')} width="30%" />
+            ) : (
+              <Split hasGutter>
+                <SplitItem>
+                  <Brand src={nvidiaLogo} alt="Nvidia logo" className="ng-mig-card__logo" />
+                </SplitItem>
+                <SplitItem>
+                  <Stack>
+                    <StackItem>
+                      <Split hasGutter>
+                        <SplitItem>
+                          <CardTitle>{selectedGPU?.modelName}</CardTitle>
+                        </SplitItem>
+                        <SplitItem>
+                          <Badge isRead>
+                            {multiInstance ? t('Multi-instance') : t('Single-instance')}
+                          </Badge>
+                        </SplitItem>
+                      </Split>
+                    </StackItem>
+                    {migStrategy && <StackItem>{`${t('MIG strategy')}: ${migStrategy}`}</StackItem>}
+                  </Stack>
+                </SplitItem>
+              </Split>
+            )}
+          </CardHeader>
+          <CardBody>
+            <div ref={containerRef} className="ng-mig-card__chart">
+              {loading ? (
+                <Skeleton screenreaderText={t('Loading card content')} />
+              ) : (
+                <ChartStack
+                  horizontal
+                  padding={0}
+                  height={15}
+                  themeColor={ChartThemeColor.multiUnordered}
+                  width={width}
+                  labelComponent={<ChartTooltip />}
+                >
+                  {chartBars}
+                </ChartStack>
+              )}
+            </div>
+          </CardBody>
+        </>
+      )}
+    </Card>
+  );
+};
+
+export default MIGCard;

src/components/GPUDashboard/Cards/MetricsCard.tsx
@@ -1,0 +1,155 @@
+import * as React from 'react';
+import { Card, CardBody, CardTitle, Skeleton, CardFooter, Bullseye } from '@patternfly/react-core';
+import {
+  usePrometheusPoll,
+  PrometheusEndpoint,
+  HumanizeResult,
+  Humanize,
+} from '@openshift-console/dynamic-plugin-sdk';
+import {
+  ChartArea,
+  ChartDonutUtilization,
+  ChartGroup,
+  ChartVoronoiContainer,
+} from '@patternfly/react-charts';
+import { Link } from 'react-router-dom';
+import {
+  getPrometheusResultScalarNumber,
+  getPrometheusResultTimeSerie,
+} from '../../../utils/prometheus';
+import { useTranslation } from '../../../i18n';
+import { timeFormatter } from '../../../utils/time';
+import { useRefWidth } from '../../../hooks/use-ref-width';
+
+import './MetricsCard.css';
+import { GPUDashboardContext } from '../GPUDashboardContext';
+
+type MetricsCardProps = {
+  query: string;
+  humanize: Humanize;
+  title: string;
+  ariaTitle: string;
+  ariaDesc: string;
+  ariaRangeTitle: string;
+  maxDomain?: number;
+  maxQuery?: string;
+  defaultUnit?: string;
+};
+
+const MetricsCard: React.FC<MetricsCardProps> = ({
+  ariaTitle,
+  title,
+  maxDomain = 0,
+  ariaDesc,
+  ariaRangeTitle,
+  humanize,
+  defaultUnit,
+  ...rest
+}) => {
+  const { t } = useTranslation();
+  const [containerRef, width] = useRefWidth();
+
+  const { selectedGPU, gpusLoaded, gpusError } = React.useContext(GPUDashboardContext);
+  const gpusErr = gpusLoaded && (!!gpusError || !selectedGPU);
+
+  // do not run the queries if there is an error
+  const [query, maxQuery] =
+    gpusErr || !gpusLoaded ? [undefined, undefined] : [`${rest.query}[60m:5m]`, rest.maxQuery];
+  const [result, queryLoaded, queryError] = usePrometheusPoll({
+    endpoint: PrometheusEndpoint.QUERY,
+    query,
+  });
+
+  const [maxResult, maxLoaded, maxError] = usePrometheusPoll({
+    endpoint: PrometheusEndpoint.QUERY,
+    // do not run the query if there is an error
+    query: maxQuery,
+  });
+
+  const timeSerie = getPrometheusResultTimeSerie(result);
+  const max = maxDomain || getPrometheusResultScalarNumber(maxResult);
+
+  const loading =
+    !gpusLoaded || !queryLoaded || !maxLoaded || !result || (!!maxQuery && !maxResult);
+
+  const error =
+    gpusErr ||
+    !!queryError ||
+    !!maxError ||
+    (!loading && (timeSerie === undefined || max === undefined));
+  const timeData = timeSerie?.map((pair) => ({
+    x: pair.time,
+    y: pair.value,
+  }));
+
+  let actualInPercents: number | undefined;
+  const latestValue = timeData?.[timeData?.length - 1].y;
+  let currentValue: HumanizeResult | undefined = undefined;
+  if (!error && latestValue !== undefined) {
+    currentValue = humanize(latestValue, defaultUnit);
+    actualInPercents = max ? (latestValue / max) * 100 : 0;
+  }
+
+  return (
+    <Card className="ng-metrics-card">
+      <CardTitle className="pf-u-text-align-center">{title}</CardTitle>
+      <CardBody>
+        {loading && !error ? (
+          <Bullseye>
+            <Skeleton shape="circle" width="70%" screenreaderText={t('Loading card content')} />
+          </Bullseye>
+        ) : (
+          <div ref={containerRef} className="ng-metrics-card__graph">
+            <ChartDonutUtilization
+              ariaDesc={title}
+              ariaTitle={ariaTitle}
+              constrainToVisibleArea={true}
+              data={error ? { x: 0 } : { x: title, y: actualInPercents }}
+              labels={({ datum }) => (datum.x ? `${title}: ${currentValue?.string}` : null)}
+              title={error ? t('Not available') : `${currentValue?.string}`}
+              thresholds={[{ value: 60 }, { value: 80 }]}
+              width={width}
+            />
+          </div>
+        )}
+      </CardBody>
+      <CardFooter className="ng-metrics-card">
+        {loading && !error ? (
+          <div style={{ height: '70px' }}>
+            <Skeleton width="100%" height="100%" screenreaderText={t('Loading card content')} />
+          </div>
+        ) : (
+          !error && (
+            <div className="ng-metrics-card__graph">
+              <Link to={`/monitoring/query-browser?query0=${rest.query}`}>
+                <ChartGroup
+                  ariaDesc={ariaDesc}
+                  ariaTitle={ariaRangeTitle}
+                  containerComponent={
+                    <ChartVoronoiContainer
+                      activateData={false}
+                      voronoiDimension="x"
+                      labels={({ datum }) =>
+                        `${
+                          humanize(datum.y, defaultUnit, currentValue?.unit).string
+                        } at ${timeFormatter.format(new Date(datum.x * 1000))}`
+                      }
+                    />
+                  }
+                  height={60}
+                  width={width}
+                  maxDomain={{ y: timeData ? Math.max(...timeData.map((td) => td.y)) * 1.2 : 0 }}
+                  padding={0}
+                >
+                  <ChartArea data={timeData} scale={{ x: 'time', y: 'linear' }} height={90} />
+                </ChartGroup>
+              </Link>
+            </div>
+          )
+        )}
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default MetricsCard;

src/components/GPUDashboard/GPUSelector.tsx
@@ -1,0 +1,53 @@
+import * as React from 'react';
+import { SelectOption, DropdownPosition, Select, Card, Spinner } from '@patternfly/react-core';
+import { GPUDashboardContext } from './GPUDashboardContext';
+import { useTranslation } from '../../i18n';
+
+const GPUSelector: React.FC = () => {
+  const { t } = useTranslation();
+  const { selectedGPU, gpus, setSelectedGPU, gpusLoaded, gpusError } =
+    React.useContext(GPUDashboardContext);
+  const [isOpen, setOpen] = React.useState(false);
+
+  const isDisabled = !gpus?.length || !!gpusError || !gpusLoaded;
+
+  let items: React.ReactElement[];
+
+  if (!gpusLoaded) {
+    items = [
+      <SelectOption key="disabled" value={t('Loading GPUs')}>
+        <Spinner size="md" /> {t('Loading GPUs')}
+      </SelectOption>,
+    ];
+  } else if (!gpus?.length || gpusError) {
+    items = [<SelectOption key="disabled" value={t('No GPU found')} />];
+  } else {
+    items = gpus.map((gpu) => (
+      <SelectOption
+        key={gpu.uuid}
+        value={gpu.uuid}
+        description={`Node: ${gpu.nodeName}, Model: ${gpu.modelName}`}
+      />
+    ));
+  }
+
+  return (
+    <Card>
+      <Select
+        onSelect={(e, selection: string) => {
+          setSelectedGPU(selection);
+          setOpen(false);
+        }}
+        onToggle={setOpen}
+        isOpen={isOpen}
+        isDisabled={isDisabled}
+        position={DropdownPosition.right}
+        selections={selectedGPU?.uuid}
+      >
+        {items}
+      </Select>
+    </Card>
+  );
+};
+
+export default GPUSelector;

src/hooks/use-gpus-info.ts
@@ -1,0 +1,55 @@
+import {
+  PrometheusEndpoint,
+  useK8sWatchResource,
+  usePrometheusPoll,
+} from '@openshift-console/dynamic-plugin-sdk';
+import * as _ from 'lodash';
+import { Node } from '../resources';
+import { useDeepCompareMemoize } from './use-deep-memoize';
+
+export type GPUInfo = {
+  uuid: string;
+  modelName: string;
+  nodeName: string;
+};
+
+/**
+ * Get a list of all GPUs in the cluster and their details.
+ *
+ */
+export const useGPUsInfo = (): [GPUInfo[], /* loaded */ boolean, /* error */ unknown] => {
+  const [result, loaded, error] = usePrometheusPoll({
+    endpoint: PrometheusEndpoint.QUERY,
+    query: 'DCGM_FI_PROF_GR_ENGINE_ACTIVE',
+  });
+
+  const gpus = useDeepCompareMemoize(
+    _.uniqBy(
+      result?.data?.result?.map((res) => ({
+        uuid: res.metric?.UUID,
+        modelName: res.metric?.modelName,
+        nodeName: res.metric?.Hostname,
+      })),
+      (g) => g.uuid,
+    ),
+  );
+
+  return [gpus, loaded, error];
+};
+
+export const useGPUNode = (gpu?: GPUInfo): [Node | undefined, boolean, unknown] => {
+  const [node, nodeLoaded, nodeError] = useK8sWatchResource<Node>(
+    gpu?.nodeName
+      ? {
+          groupVersionKind: {
+            kind: 'Node',
+            version: 'v1',
+          },
+          name: gpu.nodeName,
+          isList: false,
+        }
+      : null,
+  );
+
+  return [node, nodeLoaded, nodeError];
+};