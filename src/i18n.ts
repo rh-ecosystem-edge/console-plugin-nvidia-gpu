06d4f7f14dcf0770617d94f3ee6dca42fb1f73a1
Rastislav Wagner <rawagner@redhat.com>
2022-07-12 10:05:27 +0200

    Add project overview gpu extension (#34)

--- src/i18n.ts
+++ src/i18n.ts
@@ -10,6 +10,7 @@ import { useTranslation as useReactI18NextTranslation } from 'react-i18next';
 // t('plugin__console-plugin-nvidia-gpu~GPU Encoder/Decoder')
 // t('plugin__console-plugin-nvidia-gpu~GPUs')
 // t('plugin__console-plugin-nvidia-gpu~GPUs health')
+// t('plugin__console-plugin-nvidia-gpu~GPU pod count')
 
 export function useTranslation() {
   return useReactI18NextTranslation('plugin__console-plugin-nvidia-gpu');
